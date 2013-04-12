e3512400479d1246995516955b73da059ef35d92
alanning <alanning@gmail.com>
2013-04-12 18:27:47 -0400

    add button to trigger method call on server

--- connection-status.js
+++ connection-status.js
@@ -2,6 +2,39 @@
 
 if (Meteor.isClient) {
 
+  Session.set('timeout', null);
+
+  Template.hello.events({
+    'click button': function (event) {
+      event.preventDefault();
+
+      Meteor.call('getResponse', function (error, response) {
+        var $res,
+            timeout;
+
+        if (error) {
+
+          alert(JSON.stringify(error));
+
+        } else {
+
+          if (!Session.equals('timeout', null)) {
+            Meteor.clearTimeout(Session.get('timeout'));
+          }
+
+          $res = $('#response');
+          $res.text('Response from server: ' + response);
+          $res.show();
+          timeout = Meteor.setTimeout(function () {
+            $res.fadeOut('slow');
+          }, 2000);
+          Session.set('timeout', timeout);
+
+        }
+      });
+    }
+  });
+
   Template.hello.connectionStatus = function () {
     var status = Meteor.status();
 
@@ -11,6 +44,14 @@ if (Meteor.isClient) {
 
     return status;
   };
+}
+
+if (Meteor.isServer) {
+  Meteor.methods({
+    getResponse: function () {
+      return new Date();
+    }
+  });
 }
 
 }());
